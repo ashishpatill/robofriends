c3f2345a44b081f2e06fa1fa27eb0d7c4fbc0c6a
Ashish Pisey <ashishpisey21@gmail.com>
2023-11-17 19:17:39 +0530

    Created CardList component and Parsed robots json

---
+++ src/CardList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+    return (
+        <div>
+           {
+            robots.map(({id, name, email}, index) => {
+                return <Card key={id} id={id} name={name} email={email}/>
+            })
+           }
+        </div>
+    );
+}
+
+export default CardList;
