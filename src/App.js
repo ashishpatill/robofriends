deaba24e8fa9df4153b33e0c0737dd511752402d
Ashish Pisey <ashishpisey21@gmail.com>
2024-01-07 02:10:41 +0530

    added state binding for searchbox and CardList component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
 import { robots } from "./robots";
 import SearchBox from "./SearchBox";
+import './App.css';
 
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots={robots}/>
-        </div>
-    );
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+        console.log('constructor');
+    }
+
+    componentDidMount() {
+        //fetching robots from api webservice
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => { return response.json(); })
+            .then(users => { this.setState({robots: users}) });
+
+        // use following if want static json values from robots.js file
+        //this.setState({ robots: robots} ); 
+        console.log('componentDidMount');
+    }
+    
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robots=>{
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        console.log('render');
+        if (this.state.robots.length === 0) {
+            return <h1 className="tc">Loading...</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots}/>
+                </div>
+            );
+        }
+    }
 }
 
 export default App;
